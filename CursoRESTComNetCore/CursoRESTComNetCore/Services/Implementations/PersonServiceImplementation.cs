e25cebd45cca975ab5530282dbd1bcc196c30bc5
Daniel Pinheiro do Nascimento <daniel.pin@outlook.com>
2020-12-26 23:19:53 -0300

    Implementação PersonServiceImplementation

---
+++ CursoRESTComNetCore/CursoRESTComNetCore/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,72 @@
+﻿using CursoRESTComNetCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CursoRESTComNetCore.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < 8; i++ )
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+      
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Daniel",
+                LastName = "Pinheiro",
+                Address = "Rua Git - São Paulo - Brasil",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person Name" + i,
+                LastName = "Person LastName" + i,
+                Address = "Person Address" + i,
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
